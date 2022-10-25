91ddd7453094431a27f20d44500466729e3fa81b
Skyness <gamebrossturkiye@gmail.com>
2022-10-25 12:14:35 +0300

    User Login Screen + BONUS

---
+++ sifreilegirisprogramı/sifreilegirisprogramı/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sifreilegirisprogramı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string k_adi, sifre;
+
+            k_adi = textBox1.Text;
+            sifre = textBox2.Text;
+
+            if (k_adi == "CsharpLove" & sifre == "12345") 
+            {
+                MessageBox.Show ("Giriş Başarılı");
+                Form frm = new Form2();
+                frm.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("kullanıcı adı veya şifre yanlış");
+                textBox1.Clear();
+                textBox2.Clear();
+
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ sifreilegirisprogramı/sifreilegirisprogramı/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sifreilegirisprogramı
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double not1, not2, not3, ort;
+
+            not1 = Convert.ToDouble (textBox1.Text);
+            not2 = Convert.ToDouble(textBox2.Text);
+            not3 = Convert.ToDouble(textBox3.Text);
+
+            ort = (not1 + not2 + not3) / 3;
+
+            label4.Text = ort.ToString();
+
+            if (ort == 0 & ort == 50)
+            {
+                label4.Text = "1";
+                label6.Text = ort.ToString();
+            }   
+            else if (ort >= 50 & ort == 55)
+            {
+                label4.Text = "2";
+                label6.Text = ort.ToString();
+            }
+            else if (ort >= 55 & ort == 70)
+            {
+                label4.Text = "3";
+                label6.Text = ort.ToString();
+            }
+            else if (ort >= 70 & ort == 85)
+            {
+                label4.Text = "4";
+                label6.Text = ort.ToString();
+            }
+            else if (ort >= 85 & ort <= 100)
+            {
+                label4.Text = "5";
+                label6.Text = ort.ToString();
+            }
+
+
+
+
+
+
+        }
+    }
+}
